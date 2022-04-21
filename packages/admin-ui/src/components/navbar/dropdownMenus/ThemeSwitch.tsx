a4906c8b3dc5c1abd16ccdc41931dac0b0c449e6
pablo <mendez4a@gmail.com>
2022-04-21 14:02:38 +0200

    Use reactContext. Set responsive sidebar

--- packages/admin-ui/src/components/navbar/dropdownMenus/ThemeSwitch.tsx
+++ packages/admin-ui/src/components/navbar/dropdownMenus/ThemeSwitch.tsx
@@ -1,21 +1,17 @@
 import { BsSunFill, BsMoonFill } from "react-icons/bs";
 import React from "react";
-import { Button } from "react-bootstrap";
 import "./themeSwitch.scss";
+import { ThemeContext } from "App";
 
 export default function ThemeSwitch({
-  theme,
-  setTheme
+  toggleTheme
 }: {
-  theme: "light" | "dark";
-  setTheme: React.Dispatch<React.SetStateAction<"light" | "dark">>;
+  toggleTheme: () => void;
 }) {
+  // get context provider
+  const { theme } = React.useContext(ThemeContext);
   return (
-    <button
-      onClick={
-        theme === "dark" ? () => setTheme("light") : () => setTheme("dark")
-      }
-    >
+    <button onClick={toggleTheme}>
       {theme === "light" ? <BsMoonFill /> : <BsSunFill />}
     </button>
   );
